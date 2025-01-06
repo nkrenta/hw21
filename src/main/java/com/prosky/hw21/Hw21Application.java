7a65e2247332512575bcf525af32a3ceee8e2623
nkrenta <nkrenta@inbox.ru>
2025-01-06 16:33:43 +0300

    Init

---
+++ src/main/java/com/prosky/hw21/Hw21Application.java
@@ -0,0 +1,13 @@
+package com.prosky.hw21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw21Application.class, args);
+    }
+
+}
